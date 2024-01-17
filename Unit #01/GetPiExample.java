6526c4b64e2ce3e3239f4a83d4fdfa7c859d64cd
Nathalie Himaya <115034276+NathalieHimaya@users.noreply.github.com>
2024-01-16 21:47:10 -0800

    Delete Unit #01/GetPiExample.java

--- Unit #01/GetPiExample.java
+++
@@ -1,9 +0,0 @@
-public class GetPiExample{
-    public static void main(String[] args){
-        System.out.println("PI = " + Math.PI);
-
-        double radius = 8;
-        double circleArea = Math.PI * Math.pow(radius, 2);
-        System.out.println("Circle Area = " + circleArea);
-    }
-}
